f8c3b47eabe63abc15234de3348a1c64efda4031
aki <aki@xps>
2014-01-06 18:16:24 +0900

    csv import&export works fine

--- src/com/akjava/gwt/markdownlist/client/MarkdownDataList.java
+++ src/com/akjava/gwt/markdownlist/client/MarkdownDataList.java
@@ -1,21 +1,52 @@
 package com.akjava.gwt.markdownlist.client;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.akjava.gwt.html5.client.download.HTML5Download;
+import com.akjava.gwt.html5.client.file.File;
+import com.akjava.gwt.html5.client.file.FileUploadForm;
+import com.akjava.gwt.html5.client.file.FileUtils;
+import com.akjava.gwt.html5.client.file.FileUtils.DataURLListener;
 import com.akjava.gwt.lib.client.HeaderAndValue;
 import com.akjava.gwt.lib.client.LogUtils;
 import com.akjava.gwt.lib.client.StorageDataList;
+import com.akjava.gwt.lib.client.widget.TabInputableTextArea;
+import com.akjava.gwt.markdownlist.client.datalist.AbstractContextMenu;
 import com.akjava.gwt.markdownlist.client.datalist.SimpleDataListItemControler;
+import com.akjava.lib.common.csv.CSVReader;
+import com.akjava.lib.common.utils.CSVUtils;
+import com.akjava.lib.common.utils.ValuesUtils;
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import com.google.common.collect.FluentIterable;
 import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.MenuBar;
 import com.google.gwt.user.client.ui.TextArea;
 
+/**
+ * TODO create this as simple TextAreaDataList
+ * @author aki
+ *
+ */
 public class MarkdownDataList extends SimpleDataListItemControler{
 private TextArea textArea;
+private HeaderAndValue copiedValue;
+private HorizontalPanel uploadPanel;
+private FileUploadForm uploadForm;
 	public void setTextArea(TextArea textArea) {
 	this.textArea = textArea;
 }
@@ -30,7 +61,7 @@ private TextArea textArea;
 	
 	public MarkdownDataList(StorageDataList dataList) {
 		super(dataList);
-		textArea=new TextArea();
+		textArea=new TabInputableTextArea();
 		textArea.addChangeHandler(new ChangeHandler() {
 			
 			@Override
@@ -53,8 +84,33 @@ private TextArea textArea;
 					}
 				}
 			});
+		 
+		 
+		 getSimpleDataListWidget().setCellContextMenu(new TestContextMenu());
+		 
 	}
 
+	/**
+	 * This context menu is very primitive,so can't modified after create.used globally
+	 * TODO 
+	 * @author aki
+	 *
+	 */
+	public class TestContextMenu extends AbstractContextMenu{
+		@Override
+		public void createMenus(MenuBar menuBar) {
+			Command test1Command = new Command() {
+				  public void execute() {
+					LogUtils.log(getCurrentName());  
+				    hide(); 
+				  }
+			};
+			
+			menuBar.addItem("Test", test1Command);
+			
+		}
+	}
+	
 	@Override
 	public HeaderAndValue createSaveData(String fileName) {
 		String text=textArea.getText();
@@ -80,15 +136,47 @@ private TextArea textArea;
 		//LogUtils.log("loadData");
 		
 	}
+	
+	private String getKeyName(){
+		return "datas";
+	}
 
 	@Override
 	public void exportDatas(List<HeaderAndValue> list) {
-		LogUtils.log("exportDatas");
+		String exportText=generateExportText(list);
+		Anchor anchor=new HTML5Download().generateTextDownloadLink(exportText,getKeyName()+".txt","Download data",true);
+		getSimpleDataListWidget().add(anchor);
+	}
+	
+	private String generateExportText(List<HeaderAndValue> list){
+		List<String> lines=FluentIterable.from(list).transform(new HeadAndValueToCsvFunction()).toList();
+		String exportText=Joiner.on("\r\n").join(lines);
+		return exportText;
+	}
+	
+	public String generateExportText(){
+		List<HeaderAndValue> list=this.getDataList().getDataList();
+		return generateExportText(list);
 	}
 
 	@Override
 	public void importData() {
-		LogUtils.log("importData");
+		uploadForm = FileUtils.createSingleTextFileUploadForm(new DataURLListener() {
+			@Override
+			public void uploaded(File file, String value) {
+				List<HeaderAndValue> list=textToHeaderAndValue(value);
+				for(HeaderAndValue hv:list){
+					getDataList().addData(hv.getHeader(), hv.getData());
+				}
+				
+				uploadPanel.removeFromParent();
+				updateList();
+			}
+		}, false);
+		
+		
+		createFormPanel("Import Datas:",uploadForm);
+		getSimpleDataListWidget().add(uploadPanel);
 	}
 
 	@Override
@@ -110,17 +198,21 @@ private TextArea textArea;
 
 	@Override
 	public void copy(Object object) {
-		LogUtils.log("copy:"+object);
+		copiedValue=(HeaderAndValue) object;
 	}
 
 	@Override
 	public void paste() {
-		LogUtils.log("paste");
+		if(copiedValue!=null){
+		getDataList().addData(copiedValue.getHeader()+" copy", copiedValue.getData());
+		updateList();
+		}
 	}
 
 	@Override
 	public void recoverLastSaved(HeaderAndValue hv) {
-		LogUtils.log("recoverLastSaved");
+		copy(hv);
+		paste();
 	}
 
 	@Override
@@ -132,5 +224,36 @@ private TextArea textArea;
 	public void doDoubleClick(int clientX, int clientY) {
 		LogUtils.log("double-click:"+clientX+","+clientY);
 	}
+	
+	private void createFormPanel(String text,FileUploadForm form){
+		if(uploadPanel==null){
+		uploadPanel=new HorizontalPanel();
+		uploadPanel.add(new Label(text));
+		uploadPanel.add(form);
+		Button bt=new Button("close",new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				uploadPanel.setVisible(false);
+			}
+		});
+		uploadPanel.add(bt);
+		}else{
+			uploadPanel.setVisible(true);
+		}
+	}
 
+	//TODO move somewhere
+	public static List<HeaderAndValue> textToHeaderAndValue(String text){
+		//List<HeaderAndValue> list=new ArrayList<HeaderAndValue>();
+		CSVReader reader=new CSVReader(text,'\t');
+		try {
+			List<String[]> csvs=reader.readAll();
+			return FluentIterable.from(csvs).transform(new CsvArrayToHeadAndValueFunction()).toList();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
