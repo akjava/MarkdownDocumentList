5dd40f903b408a60e7d9aec5094782a2e24f87b2
aki <aki@xps>
2014-01-06 22:08:23 +0900

    chrome extension version

--- src/com/akjava/gwt/markdownlist/client/MarkdownDocumentList.java
+++ src/com/akjava/gwt/markdownlist/client/MarkdownDocumentList.java
@@ -7,6 +7,8 @@ import com.akjava.gwt.lib.client.datalist.TextAreaBasedDataList;
 import com.akjava.gwt.markdowneditor.client.MarkdownEditor;
 import com.google.common.base.Optional;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -28,15 +30,29 @@ public class MarkdownDocumentList implements EntryPoint {
 		
 		VerticalPanel panel2=new VerticalPanel();
 		root.add(panel2);
-		TextAreaBasedDataList mlist=new TextAreaBasedDataList(storageDataList);
+		final TextAreaBasedDataList mlist=new TextAreaBasedDataList(storageDataList);
 		panel2.add(mlist.getSimpleDataListWidget());//list-side
 		mlist.loadData(Optional.<HeaderAndValue>absent());
 		
 		
-		MarkdownEditor editor=new MarkdownEditor();
+		final MarkdownEditor editor=new MarkdownEditor();
 		panel1.add(editor);
-		mlist.addKeyUpHandler(editor.getTextArea());
+		mlist.addKeyHandler(editor.getTextArea());
 		mlist.setTextArea(editor.getTextArea());
 		
+		//add sync save action
+		editor.getTextArea().addKeyDownHandler(new KeyDownHandler() {
+			@Override
+			public void onKeyDown(KeyDownEvent event) {
+				if(event.isControlKeyDown()){
+					if(event.getNativeKeyCode()==83){//save
+						event.preventDefault();
+						mlist.getSimpleDataListWidget().save();
+						return;
+					}
+				}
+			}
+		});
+		mlist.unselect();
 	}
 }
