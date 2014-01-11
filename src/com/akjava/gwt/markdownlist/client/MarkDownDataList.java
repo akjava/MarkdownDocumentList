850dd375cdd73fa35afc447e44c03da23abd2246
aki <aki@xps>
2014-01-11 23:34:51 +0900

    improved

---
+++ src/com/akjava/gwt/markdownlist/client/MarkDownDataList.java
@@ -0,0 +1,27 @@
+package com.akjava.gwt.markdownlist.client;
+
+import com.akjava.gwt.lib.client.StorageDataList;
+import com.akjava.gwt.lib.client.datalist.SimpleTextData;
+import com.akjava.gwt.lib.client.datalist.TextAreaBasedDataList;
+import com.akjava.gwt.markdowneditor.client.MarkdownEditor;
+import com.google.common.base.Optional;
+
+public class MarkDownDataList extends TextAreaBasedDataList{
+private MarkdownEditor editor;
+	public MarkDownDataList(MarkdownEditor editor,StorageDataList dataList) {
+		super(dataList);
+		this.editor=editor;
+	}
+	@Override
+	public void loadData(Optional<SimpleTextData> hv) {
+		super.loadData(hv);
+		/**
+		 * why refresh instead of convert.
+		 * because for just check the mdtext,converting is annoying.
+		 * but in future double and load & convert would be supported
+		 */
+		editor.getHtmlArea().setText("");//fresh first;
+		editor.getPreviewHTML().setHTML("");
+	}
+
+}
--- src/com/akjava/gwt/markdownlist/client/MarkdownDocumentList.java
+++ src/com/akjava/gwt/markdownlist/client/MarkdownDocumentList.java
@@ -1,14 +1,36 @@
 package com.akjava.gwt.markdownlist.client;
 
-import com.akjava.gwt.lib.client.HeaderAndValue;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.akjava.gwt.html5.client.download.HTML5Download;
+import com.akjava.gwt.html5.client.file.Blob;
+import com.akjava.gwt.jszip.client.JSZip;
 import com.akjava.gwt.lib.client.StorageControler;
 import com.akjava.gwt.lib.client.StorageDataList;
-import com.akjava.gwt.lib.client.datalist.TextAreaBasedDataList;
+import com.akjava.gwt.lib.client.datalist.SimpleDataList;
+import com.akjava.gwt.lib.client.datalist.SimpleDataList.SimpleTextDataComparator;
+import com.akjava.gwt.lib.client.datalist.SimpleTextData;
+import com.akjava.gwt.lib.client.datalist.SimpleTextDataPredicates.SameDirectoryOnly;
 import com.akjava.gwt.markdowneditor.client.MarkdownEditor;
+import com.akjava.gwt.markdowneditor.client.MarkdownFunctions.SimpleTextDataToTitleLinkTextFunction;
+import com.akjava.gwt.markdowneditor.client.MarkdownFunctions.SimpleTextKeywordLinksFunction;
+import com.akjava.lib.common.functions.StringFunctions.StringToPreFixAndSuffix;
+import com.akjava.lib.common.utils.TemplateUtils;
+import com.google.common.base.Joiner;
 import com.google.common.base.Optional;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyDownEvent;
 import com.google.gwt.event.dom.client.KeyDownHandler;
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
@@ -30,12 +52,13 @@ public class MarkdownDocumentList implements EntryPoint {
 		
 		VerticalPanel panel2=new VerticalPanel();
 		root.add(panel2);
-		final TextAreaBasedDataList mlist=new TextAreaBasedDataList(storageDataList);
-		panel2.add(mlist.getSimpleDataListWidget());//list-side
-		mlist.loadData(Optional.<HeaderAndValue>absent());
-		
-		
 		final MarkdownEditor editor=new MarkdownEditor();
+		final MarkDownDataList mlist=new MarkDownDataList(editor,storageDataList);
+		panel2.add(mlist.getSimpleDataListWidget());//list-side
+		mlist.loadData(Optional.<SimpleTextData>absent());
+		
+		
+	
 		panel1.add(editor);
 		mlist.addKeyHandler(editor.getTextArea());
 		mlist.setTextArea(editor.getTextArea());
@@ -54,5 +77,65 @@ public class MarkdownDocumentList implements EntryPoint {
 			}
 		});
 		mlist.unselect();
+		
+	
+		
+		mlist.getSimpleDataListWidget().getOptionButtonPanel().add(new Button("zip download",new ClickHandler() {
+			@Override
+			public void onClick(ClickEvent event) {
+				List<SimpleTextData> datas=mlist.getDataList().getDataList();
+				Collections.sort(datas, new SimpleTextDataComparator(SimpleDataList.ORDER_AZ));
+				JSZip zip=JSZip.newJSZip();
+				for(SimpleTextData data:datas){
+					zip.file(data.getName()+".md", templateData(data,datas),data.getCdate());
+				}
+				
+				String keylinks=createKeywordLinks(datas);
+				zip.file("keyword.txt", keylinks);
+				
+				Blob blob=zip.generateBlob(null);
+				Anchor a=new HTML5Download().generateDownloadLink(blob,"application/zip","test.zip","download sample blob",true);
+				mlist.getSimpleDataListWidget().getVerticalOptionPanel().add(a);
+			}
+		}));
+	}
+	
+	public String createKeywordLinks(List<SimpleTextData> datas){
+		
+		
+		
+		List<List<String>> lines=FluentIterable.from(datas)
+				.transform(new SimpleTextKeywordLinksFunction("/cms/",true,".html"))
+				.toList();
+		
+		List<String> all=Lists.newArrayList();
+		for(List<String> data:lines){
+			Iterables.addAll(all, data);
+		}
+		
+		return Joiner.on("\n").join(all);
+				
+		
+	}
+	
+	
+	public String templateData(SimpleTextData data,List<SimpleTextData> datas){
+		String textData=data.getData();
+		if(textData.indexOf("${")!=-1){
+			Map<String,String> map=new HashMap<String,String>();
+			//do template
+			map.put("indexes", createIndexes(data,datas));
+			return TemplateUtils.createAdvancedText(textData, map);
+		}else{
+			return textData;
+		}
+	}
+	
+	private String createIndexes(SimpleTextData data,List<SimpleTextData> datas){
+		List<String> lines=FluentIterable.from(datas)
+		.filter(new SameDirectoryOnly(data.getName(),false))
+		.transform(new SimpleTextDataToTitleLinkTextFunction("",false,".html"))
+		.transform(new StringToPreFixAndSuffix("- ","")).toList();
+		return Joiner.on("\n").join(lines);
 	}
 }
